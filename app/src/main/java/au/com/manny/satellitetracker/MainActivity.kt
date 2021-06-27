7a72f2c1b0869edc7c2faaf42e86f45490be9c23
Manjesh Govindappa <manjesh.govindappa@ellumehealth.com>
2021-06-27 14:28:04 +1000

    Add following functionality
    - Add external libs
    - Add navigation graph and components
    - Add fragments

--- app/src/main/java/au/com/manny/satellitetracker/MainActivity.kt
+++ app/src/main/java/au/com/manny/satellitetracker/MainActivity.kt
@@ -7,5 +7,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val satellitesFragment = supportFragmentManager
+        val fragmentTransaction = satellitesFragment.beginTransaction()
+        fragmentTransaction.replace(R.id.content, FragmentSatellites.newInstance("",""))
     }
 }
